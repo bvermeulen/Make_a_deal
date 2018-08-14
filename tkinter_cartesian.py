e8e32e167307e9502776247c642096f9c2fd85d5
bvermeulen <bruno_vermeulen2001@yahoo.com>
2018-08-14 22:39:26 +0700

    including axes

--- tkinter_cartesian.py
+++ tkinter_cartesian.py
@@ -1,37 +1,173 @@
 from tkinter import ttk
 from tkinter import Tk, Canvas, Button
 from tkinter.ttk import Frame, Style, Button
-# style=ttk.Style()
+import numpy as np
 import pdb
 
 
 class TkinterCartesian():
     '''  Tkinter objects and methods: '''
 
-    def setup(self, title='...', gridx=400, gridy=300, cellx=2, celly=2):
-        '''  setup of tkinter parameters '''
-        # pdb.set_trace()
-        self.gridx=gridx
-        self.gridy=gridy
-        self.cellx=cellx
-        self.celly=celly
-        self.axiswidth=20
+    def setwindow(self, x=4, y=3, dpi=92, xmin=None, xmax=None, ymin=None,
+                  ymax=None, tickx=None, markx=None, ticky=None, marky=None):
+        ''' set the window boundary '''
+        self.x=x
+        self.y=y
+        self.dpi=dpi
+        self.pixelx = x*dpi
+        self.pixely = y*dpi
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+        self.tickx = tickx
+        self.markx = markx
+        self.ticky = ticky
+        self.marky = marky
+
+    def _cartesian(self, point):
+        '''  translate point in cartersian (x, y) to pixel grid point in
+             plotwindow (px, py)'''
+        px = (point[0]-self.xmin)/(self.xmax-self.xmin)*self.pixelx
+        py = self.pixely*(1-(point[1]-self.ymin)/(self.ymax-self.ymin))
+        px = px
+        py = py
+        return (px, py)
+
+    def plotframe(self):
+        '''  plot the frame  '''
+        self.drawline((self.xmin, self.ymin), (self.xmax, self.ymin),
+                      color='white', width=1)
+        self.drawline((self.xmin, self.ymax), (self.xmax, self.ymax),
+                      color='white', width=1)
+        self.drawline((self.xmin, self.ymin), (self.xmin, self.ymax),
+                      color='white', width=1)
+        self.drawline((self.xmax, self.ymin), (self.xmax, self.ymax),
+                      color='white', width=1)
+
+    def grid(self):
+        '''  if display is not centered display grid otherwise display
+             display x and y axis '''
+        if self.tickx:
+            ticks = np.arange(self.xmin, self.xmax, self.tickx)
+            for x in ticks:
+                self.drawline((x, self.ymin), (x, self.ymax),
+                              color='grey', width=1)
+
+        if self.ticky:
+            ticks = np.arange(self.ymin, self.ymax, self.ticky)
+            for y in ticks:
+                self.drawline((self.xmin, y), (self.xmax, y),
+                              color='grey', width=1)
+
+    def plotcell(self, point, color='white', center=False,
+                  size=(2, 2), shape='rectangle'):
+        ''' plots a color cell at grid point (x, y) '''
+
+        if not center:
+            point = list(point)
+            point[0] = point[0] + 0.5
+            point[1] = point[1] + 0.5
+
+        ppoint = self._cartesian(point)
+
+        bbox = (int(round(ppoint[0]-size[0]/2)),
+                int(round(ppoint[1]-size[1]/2)),
+                int(round(ppoint[0]+size[0]/2)),
+                int(round(ppoint[1]+size[1]/2)))
+
+        if shape == 'oval':
+            self.pw.create_oval(bbox, fill=color, width=0)
+
+        elif shape == 'rectangle':
+            self.pw.create_rectangle(bbox, fill=color, width=0)
+
+        else:
+            raise ValueError("shape is either 'oval' or 'rectangle'")
+
+    def drawline(self, point1, point2, color='white', width=2):
+        '''  draws a line from point1 to point2 '''
+        ppoint2 = self._cartesian(point2)
+        if not point1:
+            ppoint1 = ppoint2
+
+        else:
+            ppoint1 = self._cartesian(point1)
+
+        self.pw.create_line(ppoint1[0], ppoint1[1], ppoint2[0], ppoint2[1],
+                            fill=color, width=width)
+
+    def refresh(self):
+        '''  refresh the screen '''
+        self.pw.delete('all')
+
+    def xaxis(self):
+        '''  draw x-axis '''
+        if self.tickx:
+            ticks = np.arange(self.xmin, self.xmax, self.tickx)
+            ticksize=4
+            for x in ticks:
+                ppoint = list(self._cartesian((x, 0)))
+                ppoint[0] = ppoint[0]+self.axiswidth
+                self.aw.create_line((ppoint[0], self.pixely+1),
+                                    (ppoint[0], self.pixely+1+ticksize),
+                                    fill='white', width=1)
+
+        if self.markx:
+            marks = np.arange(self.xmin, self.xmax, self.markx)
+            ticksize=8
+            for x in marks:
+                if self.markx < 10:
+                    _num = str(f'{float(x):.1f}')
+                else:
+                    _num = str(f'{x}')
+                ppoint = list(self._cartesian((x, 0)))
+                ppoint[0] = ppoint[0]+self.axiswidth
+                self.aw.create_text(ppoint[0], self.pixely+20, text=_num,
+                                    fill='white')
+                self.aw.create_line((ppoint[0], self.pixely+1),
+                                    (ppoint[0], self.pixely+1+ticksize),
+                                    fill='white', width=1)
+
+    def yaxis(self):
+        '''  draw y-axis '''
+        if self.ticky:
+            ticks = np.arange(self.ymin, self.ymax, self.ticky)
+            ticksize=4
+            for y in ticks:
+                ppoint = self._cartesian((0, y))
+                self.aw.create_line((self.axiswidth-1, ppoint[1]),
+                                    (self.axiswidth-1-ticksize, ppoint[1]),
+                                    fill='white', width=1)
+
+        if self.marky:
+            marks = np.arange(self.ymin, self.ymax, self.marky)
+            ticksize=8
+            for y in marks:
+                if self.marky < 10:
+                    _num = str(f'{float(y):.1f}')
+                else:
+                    _num = str(f'{y}')
+
+                ppoint = self._cartesian((0, y))
+                self.aw.create_text(self.axiswidth-25, ppoint[1], text=_num,
+                                    fill='white')
+                self.aw.create_line((self.axiswidth-1, ppoint[1]),
+                                    (self.axiswidth-1-ticksize, ppoint[1]),
+                                    fill='white', width=1)
+
+    def setuptk(self, title='...'):
+        self.axiswidth=46
         self.padding = 2
-        self.aframe = (self.gridx*self.cellx, self.gridy*self.celly)
-        self.dframe = (self.aframe[0]+self.axiswidth+self.padding, 50)
-        self.cframe = (50, self.aframe[1]+self.axiswidth+self.padding)
+        self.aframe = (self.pixelx+self.axiswidth, self.pixely+self.axiswidth)
+        self.pframe = (self.pixelx, self.pixely)
+        self.dframe = (self.aframe[0]+self.padding, 46)
+        self.cframe = (46, self.aframe[1]+self.padding)
         self.root = Tk()
         self.root.title(title)
         self.root.configure(background='darkblue')
         style = ttk.Style()
         self.exit=False
-
-        style.configure("M.TFrame", background='grey')
-        self.mainframe = ttk.Frame(self.root,
-                                   borderwidth=2,
-                                   style="M.TFrame")
-        self.mainframe.grid(row=0, column=0, padx=self.padding,
-                            pady=self.padding)
 
         style.configure("B.TFrame", background='yellow')
         self.displayframe = ttk.Frame(self.root,
@@ -50,114 +186,17 @@ class TkinterCartesian():
         self.controlframe.grid(row=0, column=1, padx=self.padding,
                                pady=self.padding, sticky='n')
 
-        style.configure("M.TFrame", background='grey')
-        self.actionframe = ttk.Frame(self.mainframe,
-                                     width=self.aframe[0],
-                                     height=self.aframe[1],
-                                     borderwidth=0,
-                                     style='M.TFrame',)
-        self.actionframe.grid(row=0, column=1, sticky='ne')
-
-        style.configure("A.TFrame", background='green')
-        self.yaxisframe = ttk.Frame(self.mainframe,
-                                    width=self.axiswidth,
-                                    height=self.aframe[1],
-                                    borderwidth=0,
-                                    style='A.TFrame')
-        self.yaxisframe.grid(row=0, column=0)
-
-        self.xaxisframe = ttk.Frame(self.mainframe,
-                                    width=self.aframe[0],
-                                    height=self.axiswidth,
-                                    borderwidth=0,
-                                    style='A.TFrame')
-        self.xaxisframe.grid(row=1, column=1)
-
-        self.aw = Canvas(self.actionframe,
+        self.aw = Canvas(self.root,
                          width=self.aframe[0], height=self.aframe[1],
                          bg='black', bd=0)
-        self.aw.pack()
+        self.aw.grid(row=0, column=0, padx=self.padding,
+                               pady=self.padding, sticky='nw')
 
-        self.CENTER = False
-        self.grid_x = list((x for x in range(0, self.aframe[0]+self.cellx,
-                            self.cellx)))
-        self.grid_y = list((y for y in range(0, self.aframe[1]+self.celly,
-                            self.celly)))
-
-    def setorigin(self, center=False):
-        '''  set origin, if CENTER is True then origin is at the center of
-             the window, if False center is in left bottom corner. '''
-        self.CENTER = center
-
-    def _cartesian(self, point):
-        '''  translated the grid origin to normal cartersian '''
-        if self.CENTER:
-            point = (int(round(self.gridx/2 + point[0],0)),
-                     int(round(self.gridy/2 - point[1],0)))
-        else:
-            point = (point[0], int(round(self.gridy - point[1],0)))
-        return point
-
-    def plotgrid(self, gridlines=False):
-        '''  if display is not centered display grid otherwise display
-             display x and y axis '''
-        if self.CENTER:
-            self.drawline((0, self.gridy/2), (0, -self.gridy/2),
-                          color='grey', width=1)
-            self.drawline((self.gridx/2, 0), (-self.gridx/2, 0),
-                          color='grey', width=1)
-
-        if gridlines:
-            for x in self.grid_x:
-                self.aw.create_line(x, 0, x, self.aframe[1],
-                                    fill='grey', width=1)
-
-            for y in self.grid_y:
-                self.aw.create_line(0, y, self.aframe[0], y,
-                                    fill='grey', width=1)
-
-    def colorcell(self, point, color='white', center=False,
-                  size=(2, 2), shape='oval'):
-        ''' plots a color cell at grid point (x, y) '''
-        point = self._cartesian(point)
-        if center:
-            offset=(0, 0)
-        else:
-            offset=(+self.cellx/2, -self.celly/2)
-
-        bbox = (int(round(point[0]*self.cellx+offset[0]-size[0]/2)),
-                int(round(point[1]*self.celly+offset[1]-size[1]/2)),
-                int(round(point[0]*self.cellx+offset[0]+size[0]/2)),
-                int(round(point[1]*self.celly+offset[1]+size[1]/2)))
-
-        if shape == 'oval':
-            self.aw.create_oval(bbox, fill=color, width=0)
-
-        elif shape == 'rectangle':
-            self.aw.create_rectangle(bbox, fill=color, width=0)
-
-        else:
-            raise ValueError("shape is either 'oval' or 'rectangle'")
-
-    def drawline(self, point1, point2, color='white', width=2):
-        '''  draws a line from point1 to point2 '''
-        point1 = (self._cartesian(point1)[0]*self.cellx,
-                  self._cartesian(point1)[1]*self.celly)
-
-        point2 = (self._cartesian(point2)[0]*self.cellx,
-                  self._cartesian(point2)[1]*self.celly)
-
-        self.aw.create_line(point1, point2, fill=color, width=width)
-
-    def refresh(self):
-        '''  refresh the screen '''
-        self.aw.delete('all')
-
-    # def getvalue(self):
-    #     '''  get the rgb value of a cell '''
-
-    # def xaxis(self, tick=10, mark=10, side='bottom'):
-    #     '''  draw xaxis
+        self.pw = Canvas(self.root,
+                         width=self.pframe[0], height=self.pframe[1],
+                         bg='black', bd=0)
+        self.pw.grid(row=0, column=0, padx=self.padding,
+                               pady=self.padding, sticky='ne')
 
     def controls(self):
         '''  define control buttons and action '''
@@ -169,6 +208,6 @@ class TkinterCartesian():
     def exit_program(self):
         '''  leave the program '''
         print(f'we will leave the program now ...')
-        self.root.after(1000)
+        self.root.after(100)
         self.exit=True
         self.root.destroy()
